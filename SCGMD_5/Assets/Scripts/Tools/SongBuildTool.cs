66902345572515438dbf6fe4c88175549d07dea6
Christoph Marschott <christoph.marschott@gmx.net>
2016-05-11 06:29:32 +0200

    Tool improvements + fixed index out of range
    More efficient and cleaner OnInspectorUpdate()
    Tool now prompts to confirm reset + load

--- SCGMD_5/Assets/Scripts/Tools/SongBuildTool.cs
+++ SCGMD_5/Assets/Scripts/Tools/SongBuildTool.cs
@@ -17,7 +17,7 @@ public class ReadOnlyDrawer : PropertyDrawer {
 		int i = label.text.IndexOf (" ");
 		EditorGUIUtility.labelWidth = 45.0f;
 		{
-			EditorGUI.PropertyField (position, property, new GUIContent (label.text.Substring (i)), true);
+			EditorGUI.PropertyField (position, property, new GUIContent (label.text.Substring (i + 1)), true);
 		}
 		EditorGUIUtility.labelWidth = 150.0f;
 		GUI.enabled = true;
@@ -117,41 +117,46 @@ public class SongBuildTool : EditorWindow {
 	}
 
 	void OnInspectorUpdate() {
-		bool isPlaying = IsPlaying (availableSongs [selectedSong - 1]) && !isPaused;
-
 		if (oldSelectedSong != selectedSong) this.SwitchSong ();
 
-		if (oldAudioPosition == audioPosition) {
-			if (isPlaying) audioPosition = GetSamplePosition (availableSongs [selectedSong - 1]) / availableSongs [selectedSong - 1].frequency;
-		} else {
-			audioSamplePosition = audioPosition * availableSongs [selectedSong - 1].frequency;
-			audioNotePosition = audioSamplePosition / 10000;
 
-			SetSamplePosition (availableSongs [selectedSong - 1], audioSamplePosition);
+		int action = 0;
+		if (oldAudioPosition       != audioPosition)       action += 1;
+		if (oldAudioSamplePosition != audioSamplePosition) action += 2;
+		if (oldAudioNotePosition   != audioNotePosition)   action += 4;
+
+		switch (action) {
+			case 0:
+				if (IsPlaying (availableSongs [selectedSong - 1]) && !isPaused) {
+					audioSamplePosition = GetSamplePosition (availableSongs [selectedSong - 1]);
+					audioPosition       = audioSamplePosition / availableSongs [selectedSong - 1].frequency;
+					audioNotePosition   = audioSamplePosition / 10000;
+				} break;
+			case 1:
+				audioSamplePosition = audioPosition       * availableSongs [selectedSong - 1].frequency;
+				audioNotePosition   = audioSamplePosition / 10000; break;
+			case 2: case 3: case 6: case 7:
+				audioPosition       = audioSamplePosition / availableSongs [selectedSong - 1].frequency;
+				audioNotePosition   = audioSamplePosition / 10000; break;
+			case 4: case 5:
+				audioSamplePosition = audioNotePosition   * 10000;
+				audioPosition       = audioSamplePosition / availableSongs [selectedSong - 1].frequency; break;
 		}
 
-		if (oldAudioSamplePosition == audioSamplePosition) {
-			if (isPlaying) audioSamplePosition = GetSamplePosition (availableSongs [selectedSong - 1]);
-		} else {
-			audioPosition = audioSamplePosition / availableSongs [selectedSong - 1].frequency;
-			audioNotePosition = audioSamplePosition / 10000;
+		if (action > 0) SetSamplePosition (availableSongs [selectedSong - 1], audioSamplePosition);
 
-			SetSamplePosition (availableSongs [selectedSong - 1], audioSamplePosition);
-		}
 
-		if (oldAudioNotePosition == audioNotePosition) {
-			if (isPlaying) audioNotePosition = GetSamplePosition (availableSongs [selectedSong - 1]) / 10000;
-		} else {
-			audioSamplePosition = audioNotePosition * 10000;
-			audioPosition = audioSamplePosition / availableSongs [selectedSong - 1].frequency;
-
-			SetSamplePosition (availableSongs [selectedSong - 1], audioSamplePosition);
-		}
-
-		oldSelectedSong = selectedSong;
-		oldAudioPosition = audioPosition;
+		oldSelectedSong        = selectedSong;
+		oldAudioPosition       = audioPosition;
 		oldAudioSamplePosition = audioSamplePosition;
-		oldAudioNotePosition = audioNotePosition;
+		oldAudioNotePosition   = audioNotePosition;
+
+
+		if (!IsPlaying(availableSongs[selectedSong - 1])) {
+			isPaused = false;
+			playPauseLabel = "►";
+		}
+
 
 		Repaint ();
 	}
@@ -162,17 +167,19 @@ public class SongBuildTool : EditorWindow {
 			GUILayout.BeginHorizontal ();
 			{
 				if (GUILayout.Button ("...", GUILayout.MaxWidth (30.0f))) {
-					string newSongPath = EditorUtility.OpenFolderPanel ("Choose Music Folder", "Assets/", "");
+					if (EditorUtility.DisplayDialog ("Choose music folder?", "Choosing a new music folder will reload all songs!\nAll unsaved data will be lost!", "Choose folder", "Cancel")) {
+						string newSongPath = EditorUtility.OpenFolderPanel ("Choose music folder", "Assets/", "");
 
-					if (newSongPath != "") {
-						if (newSongPath.Contains (Application.dataPath)) {
-							int i = newSongPath.IndexOf ("Assets");
+						if (newSongPath != "") {
+							if (newSongPath.Contains (Application.dataPath)) {
+								int i = newSongPath.IndexOf ("Assets");
 
-							songPath = newSongPath.Substring (i);
+								songPath = newSongPath.Substring (i);
 						
-							this.FindSongs ();
-						} else {
-							EditorUtility.DisplayDialog ("Error", "Chosen path is not in Assets folder", "OK");
+								this.FindSongs ();
+							} else {
+								EditorUtility.DisplayDialog ("Error", "Chosen path is not in Assets folder", "OK");
+							}
 						}
 					}
 				}
@@ -185,8 +192,10 @@ public class SongBuildTool : EditorWindow {
 			{
 				EditorGUILayout.Space ();
 
-				if (GUILayout.Button ("Update Song List", GUILayout.MaxWidth (150.0f))) {
-					this.FindSongs ();
+				if (GUILayout.Button ("Update song list", GUILayout.MaxWidth (150.0f))) {
+					if (EditorUtility.DisplayDialog ("Update song list?", "Updating the song list will reload all songs!\nAll unsaved data will be lost!", "Update", "Cancel")) {
+						this.FindSongs ();
+					}
 				}
 
 				EditorGUILayout.Space ();
@@ -234,21 +243,12 @@ public class SongBuildTool : EditorWindow {
 			EditorGUILayout.Space ();
 			EditorGUILayout.Space ();
 
-			this.DisplayNoteEditor (0);
+			for (int i = 0; i < 4; ++i) {
+				this.DisplayNoteEditor (i);
 
-			EditorGUILayout.Space ();
+				EditorGUILayout.Space ();
+			}
 
-			this.DisplayNoteEditor (1);
-
-			EditorGUILayout.Space ();
-
-			this.DisplayNoteEditor (2);
-
-			EditorGUILayout.Space ();
-
-			this.DisplayNoteEditor (3);
-
-			EditorGUILayout.Space ();
 			EditorGUILayout.Space ();
 
 			GUILayout.BeginHorizontal ();
@@ -261,7 +261,7 @@ public class SongBuildTool : EditorWindow {
 					if (audioNotePosition >= noteJump) {
 						audioNotePosition -= noteJump;
 					} else {
-						audioNotePosition = 0;
+						this.ResetAudioPosition ();
 					}
 				}
 
@@ -311,21 +311,25 @@ public class SongBuildTool : EditorWindow {
 				EditorGUILayout.Space ();
 
 				if (GUILayout.Button (new GUIContent("Reset song", "Resets all notes in current song"), GUILayout.MaxWidth (100.0f))) {
-					this.ResetNotes (selectedSong - 1);
+					if (EditorUtility.DisplayDialog ("Reset " + availableSongs[selectedSong - 1].name + "?", "Are you sure you want to reset all notes in " + availableSongs[selectedSong - 1].name + "?", "Reset", "Cancel")) {
+						this.ResetNotes (selectedSong - 1);
 
-					audioSamplePosition = 0;
+						this.ResetAudioPosition ();
+					}
 				}
 
 				if (GUILayout.Button (new GUIContent("Reset all songs", "Resets all notes in all songs"), GUILayout.MaxWidth (100.0f))) {
-					for (int i = 0; i < availableSongs.Length; ++i) {
-						this.ResetNotes (i, true);
+					if (EditorUtility.DisplayDialog ("Reset all songs?", "Are you sure you want to reset all notes in all songs?", "Reset", "Cancel")) {
+						for (int i = 0; i < availableSongs.Length; ++i) {
+							this.ResetNotes (i);
 
-						EditorUtility.DisplayProgressBar ("Resetting notes:", availableSongs [i].name, Mathf.InverseLerp (0, availableSongs.Length, i));
+							EditorUtility.DisplayProgressBar ("Resetting notes:", availableSongs [i].name, Mathf.InverseLerp (0, availableSongs.Length, i));
+						}
+
+						EditorUtility.ClearProgressBar ();
+
+						this.ResetAudioPosition ();
 					}
-
-					EditorUtility.ClearProgressBar ();
-
-					audioSamplePosition = 0;
 				}
 				GUILayout.Label (GUI.tooltip, GUILayout.MaxWidth (200.0f));
 			}
@@ -348,7 +352,9 @@ public class SongBuildTool : EditorWindow {
 				}
 
 				if (GUILayout.Button ("Load song")) {
-					this.LoadSong (selectedSong - 1);
+					if (EditorUtility.DisplayDialog ("Load " + availableSongs[selectedSong - 1].name + "?", "Are you sure you want to load " + availableSongs[selectedSong - 1].name + "?\nAll unsaved data will be lost!", "Load", "Cancel")) {
+						this.LoadSong (selectedSong - 1);
+					}
 				}
 
 				EditorGUILayout.Space ();
@@ -358,7 +364,7 @@ public class SongBuildTool : EditorWindow {
 
 				if (GUILayout.Button ("Save all")) {
 					for (int i = 0; i < availableSongs.Length; ++i) {
-						this.SaveSong (i, true);
+						this.SaveSong (i);
 						EditorUtility.DisplayProgressBar ("Saving notes", availableSongs[i].name, Mathf.InverseLerp(0, availableSongs.Length, i));
 					}
 
@@ -366,12 +372,14 @@ public class SongBuildTool : EditorWindow {
 				}
 
 				if (GUILayout.Button ("Load all")) {
-					for (int i = 0; i < availableSongs.Length; ++i) {
-						this.LoadSong (i, true);
-						EditorUtility.DisplayProgressBar ("Loading notes", availableSongs[i].name, Mathf.InverseLerp(0, availableSongs.Length, i));
-					}
+					if (EditorUtility.DisplayDialog ("Load all songs?", "Are you sure you want to load all songs?\nAll unsaved data will be lost!", "Load", "Cancel")) {
+						for (int i = 0; i < availableSongs.Length; ++i) {
+							this.LoadSong (i);
+							EditorUtility.DisplayProgressBar ("Loading notes", availableSongs [i].name, Mathf.InverseLerp (0, availableSongs.Length, i));
+						}
 
-					EditorUtility.ClearProgressBar ();
+						EditorUtility.ClearProgressBar ();
+					}
 				}
 			}
 			GUILayout.EndHorizontal ();
@@ -392,7 +400,7 @@ public class SongBuildTool : EditorWindow {
 
 						this.SwitchSong ();
 					} else {
-						audioPosition = 0;
+						this.ResetAudioPosition ();
 					}
 				}
 
@@ -439,11 +447,11 @@ public class SongBuildTool : EditorWindow {
 		{
 			EditorGUILayout.Space ();
 			EditorGUILayout.Space ();
-			if (GUILayout.Button("Add Note", GUILayout.MaxWidth (100.0f))) {
+			if (GUILayout.Button("Add note", GUILayout.MaxWidth (100.0f))) {
 				notes [selectedSong - 1] [audioNotePosition, i] = 0;
 			}
 
-			if (GUILayout.Button("Add Hold Note", GUILayout.MaxWidth (100.0f))) {
+			if (GUILayout.Button("Add hold note", GUILayout.MaxWidth (100.0f))) {
 				notes [selectedSong - 1] [audioNotePosition, i] = longNoteLength;
 			}
 
@@ -475,7 +483,7 @@ public class SongBuildTool : EditorWindow {
 			}
 			EditorGUIUtility.labelWidth = 150.0f;
 
-			if (GUILayout.Button("Remove Note", GUILayout.MaxWidth (100.0f))) {
+			if (GUILayout.Button("Remove note", GUILayout.MaxWidth (100.0f))) {
 				notes [selectedSong - 1] [audioNotePosition, i] = -1;
 			}
 
@@ -518,41 +526,33 @@ public class SongBuildTool : EditorWindow {
 			notes.Add (new int[noteCount [i] + 1, 4]);
 
 			if (this.CheckSave (i)) {
-				this.LoadSong (i, true);
-
-				EditorUtility.DisplayProgressBar ("Loading notes", availableSongs[i].name, Mathf.InverseLerp(0, availableSongs.Length, i));
+				this.LoadSong (i);
 			} else {
-				this.ResetNotes (i, true);
-
-				EditorUtility.DisplayProgressBar ("Resetting notes:", availableSongs [i].name, Mathf.InverseLerp (0, availableSongs.Length, i));
+				this.ResetNotes (i);
 			}
 		}
-
-		EditorUtility.ClearProgressBar ();
 	}
 
-	private void ResetNotes(int i, bool resetAll = false) {
+	private void ResetNotes(int i) {
 		for (int j = 0; j < notes [i].GetLength (0); ++j) {
 			for (int k = 0; k < notes [i].GetLength (1); ++k) {
 				notes [i] [j, k] = -1;
 			}
 
-			if (!resetAll) EditorUtility.DisplayProgressBar ("Resetting notes:", availableSongs [i].name, Mathf.InverseLerp (0, notes [i].GetLength (0), j));
+			if (j % 4 == 0) EditorUtility.DisplayProgressBar ("Resetting notes", availableSongs [i].name, Mathf.InverseLerp (0, notes [i].GetLength (0), j));
 		}
 
-		if (!resetAll) EditorUtility.ClearProgressBar ();
+		EditorUtility.ClearProgressBar ();
 	}
 
 	private void SwitchSong() {
+		this.ResetAudioPosition ();
+
 		if (IsPlaying (availableSongs [selectedSong - 1])) {
 			StopAllClips ();
 
-			this.ResetAudioPosition ();
-
 			PlayPauseSong (availableSongs [selectedSong - 1]);
 		}
-
-		audioPosition = 0;
 	}
 
 	private void ResetAudioPosition() {
@@ -562,7 +562,7 @@ public class SongBuildTool : EditorWindow {
 	}
 
 
-	public void SaveSong(int song, bool saveAll = false) {
+	public void SaveSong(int song) {
 		BinaryFormatter binaryFormatter = new BinaryFormatter ();
 
 		FileStream file = File.Open (Application.persistentDataPath + "/" + availableSongs [song].name + "_notes.dat", FileMode.Create);
@@ -574,17 +574,17 @@ public class SongBuildTool : EditorWindow {
 					noteContainer.notes [i, j] = notes [song] [i, j];
 				}
 
-				if (!saveAll) EditorUtility.DisplayProgressBar ("Saving notes", availableSongs [song].name, Mathf.InverseLerp (0, notes [song].GetLength (0), i));
+				if (i % 4 == 0) EditorUtility.DisplayProgressBar ("Saving notes", availableSongs [song].name, Mathf.InverseLerp (0, notes [song].GetLength (0), i));
 			}
 
-			if (!saveAll) EditorUtility.ClearProgressBar ();
+			EditorUtility.ClearProgressBar ();
 
 			binaryFormatter.Serialize (file, noteContainer);
 		}
 		file.Close ();
 	}
 
-	public void LoadSong(int song, bool saveAll = false) {
+	public void LoadSong(int song) {
 		if (this.CheckSave(song)) {
 			BinaryFormatter binaryFormatter = new BinaryFormatter ();
 			NoteContainer noteContainer;
@@ -600,10 +600,10 @@ public class SongBuildTool : EditorWindow {
 					notes [song] [i, j] = noteContainer.notes [i, j];
 				}
 
-				if (!saveAll) EditorUtility.DisplayProgressBar ("Loading notes", availableSongs [song].name, Mathf.InverseLerp (0, noteContainer.notes.GetLength (0), i));
+				if (i % 4 == 0) EditorUtility.DisplayProgressBar ("Loading notes", availableSongs [song].name, Mathf.InverseLerp (0, noteContainer.notes.GetLength (0), i));
 			}
 
-			if (!saveAll) EditorUtility.ClearProgressBar ();
+			EditorUtility.ClearProgressBar ();
 		}
 	}
 
